5a0563f3ae62aed3fbd9bafeb48936609383b74e
MagisterVitae <matteo.serina89@gmail.com>
2016-05-31 23:13:59 +0200

    Impostato pulsante di configurazione. Iniziato pulsante di refresh.

--- app/src/main/java/sturmtruppen/com/trafficwidget/TrafficWidget.java
+++ app/src/main/java/sturmtruppen/com/trafficwidget/TrafficWidget.java
@@ -3,11 +3,14 @@ package sturmtruppen.com.trafficwidget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.widget.RemoteViews;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
@@ -15,36 +18,71 @@ import android.widget.Toast;
  */
 public class TrafficWidget extends AppWidgetProvider {
 
+    public static String ACTION_WIDGET_CONFIGURE = "ConfigureWidget";
+    public static String ACTION_WIDGET_REFRESH = "sturmtruppen.com.trafficwidget.MANUAL_REFRESH";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = TrafficWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+       /* CharSequence widgetText = TrafficWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.traffic_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(appWidgetId, views);*/
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy  hh:mm:ss a");
+        String currentTime = formatter.format(new Date());
+        String strWidgetText = currentTime;
+
+        RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.traffic_widget);
+        updateViews.setTextViewText(R.id.widgettext, "[" + String.valueOf(appWidgetId) + "]" + strWidgetText);
+
+        // Sets up the settings button to open the configuration activity
+        Intent configIntent = new Intent(context, TrafficWidgetConfigureActivity.class);
+        configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        PendingIntent configPendingIntent = PendingIntent.getActivity(context, appWidgetId, configIntent, 0);
+        updateViews.setOnClickPendingIntent(R.id.btnConfig, configPendingIntent);
+        configIntent.setAction(ACTION_WIDGET_CONFIGURE + Integer.toString(appWidgetId));
+
+        Intent refreshIntent = new Intent(context, TrafficWidgetIntentReceiver.class);
+        refreshIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        PendingIntent refreshPendingIntent = buildRefreshPendingIntent(context);
+        updateViews.setOnClickPendingIntent(R.id.btnRefresh, refreshPendingIntent);
+        refreshIntent.setAction(ACTION_WIDGET_REFRESH);
+
+        appWidgetManager.updateAppWidget(appWidgetId, updateViews);
+
+        Toast.makeText(context, "updateAppWidget(): " + String.valueOf(appWidgetId) + "\n" + strWidgetText, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+        int i = 1;
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            //updateAppWidget(context, appWidgetManager, appWidgetId);
-            int currentWidgetId = appWidgetId;
-            String url = "http://www.tutorialspoint.com";
+            updateAppWidget(context, appWidgetManager, appWidgetId);
 
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse(url));
+            /*String currentTime = formatter.format(new Date());
+            strWidgetText = strWidgetText + "\n" + currentTime;
 
-            PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.traffic_widget);
+            RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.traffic_widget);
+            updateViews.setTextViewText(R.id.widgettext, strWidgetText);
+            appWidgetManager.updateAppWidget(appWidgetIds, updateViews);
 
-            views.setOnClickPendingIntent(R.id.button, pending);
-            appWidgetManager.updateAppWidget(currentWidgetId, views);
-            Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
+            super.onUpdate(context, appWidgetManager, appWidgetIds);
+            Toast.makeText(context, "onUpdate()", Toast.LENGTH_LONG).show();*/
+
+            RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.traffic_widget);
+            updateViews.setOnClickPendingIntent(R.id.btnRefresh, buildRefreshPendingIntent(context));
+
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+            Toast.makeText(context, "onUpdate(): " + String.valueOf(i) + " : " + String.valueOf(appWidgetId), Toast.LENGTH_LONG).show();
+            i++;
         }
     }
 
@@ -54,16 +92,33 @@ public class TrafficWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             TrafficWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
         }
+        Toast.makeText(context, "onDeleted()", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        Toast.makeText(context, "onEnabled()", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+        Toast.makeText(context, "onDisabled()", Toast.LENGTH_LONG).show();
     }
+
+    public static PendingIntent buildRefreshPendingIntent(Context context) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_WIDGET_REFRESH);
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public static void pushWidgetUpdate(Context context, RemoteViews remoteViews) {
+        ComponentName myWidget = new ComponentName(context, TrafficWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(myWidget, remoteViews);
+    }
+
+
 }
 
